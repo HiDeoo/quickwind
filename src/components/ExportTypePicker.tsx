e24d0d84e434aef64620157c2ac51ca03a5e8ff4
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-09-15 14:29:53 +0200

    feat: add focus rings

--- src/components/ExportTypePicker.tsx
+++ src/components/ExportTypePicker.tsx
@@ -1,4 +1,5 @@
 import { useStore } from '@nanostores/preact'
+import clsx from 'clsx'
 
 import { getExportTypes, type ExportCategory } from '../libs/export'
 import { currentExportTypes } from '../store'
@@ -18,8 +19,18 @@ export function ExportTypePicker({ category }: ExportTypePickerProps) {
 
   return (
     <label>
-      Export type:
-      <select value={$currentExportTypes[category]} onChange={handleChange}>
+      Export:
+      <select
+        className={clsx(
+          'ml-2 px-1.5 py-1 min-w-[7rem] rounded bg-stone-300/50 dark:bg-stone-700/75 outline-none',
+          'hover:bg-stone-300 dark:hover:bg-stone-700',
+          'focus-visible:ring-2 focus-visible:ring-offset-1',
+          'focus-visible:ring-indigo-500 focus-visible:ring-offset-stone-50',
+          'dark:focus-visible:ring-indigo-400 dark:focus-visible:ring-offset-stone-900'
+        )}
+        onChange={handleChange}
+        value={$currentExportTypes[category]}
+      >
         {exportTypes.map((exportType) => (
           <option key={exportType} value={exportType}>
             {exportType}
