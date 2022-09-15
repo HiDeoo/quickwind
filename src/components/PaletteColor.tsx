214b714cc999446afe270ae098811c00c7ba9ccb
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-09-15 17:36:37 +0200

    feat: add copy indicator

--- src/components/PaletteColor.tsx
+++ src/components/PaletteColor.tsx
@@ -2,6 +2,7 @@ import { useStore } from '@nanostores/preact'
 import clsx from 'clsx'
 import { colord, extend } from 'colord'
 import a11yPlugin from 'colord/plugins/a11y'
+import { useState } from 'preact/hooks'
 
 import { getFormattedColor } from '../libs/export'
 import { type ColorName } from '../libs/tailwind'
@@ -11,22 +12,34 @@ extend([a11yPlugin])
 
 export function PaletteColor({ color, name, shade }: PaletteColorProps) {
   const $currentExportType = useStore(currentExportTypes)['colors']
+  const [copied, setCopied] = useState(false)
 
   const cColor = colord(color)
   const textColor = cColor.isLight() ? cColor.darken(1) : cColor.lighten(1)
 
   const formattedColor = getFormattedColor($currentExportType, cColor, name, shade)
 
-  function handleClick() {
-    navigator.clipboard.writeText(formattedColor)
+  async function handleClick() {
+    try {
+      await navigator.clipboard.writeText(formattedColor)
+
+      setCopied(true)
+
+      setTimeout(() => {
+        setCopied(false)
+      }, 350)
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (
     <button
       className={clsx(
-        'w-full h-full motion-safe:transition-transform text-[0] outline-none font-bold active:scale-150',
+        'w-full h-full motion-safe:transition-transform text-[0] outline-none font-bold ease-out',
         'hover:text-[0.625rem] hover:scale-125 hover:border-2 hover:border-stone-50 dark:hover:border-stone-900 hover:z-10',
-        'focus-visible:text-sm focus-visible:scale-125 focus-visible:border-2 focus-visible:border-stone-900 focus-visible:z-10'
+        'focus-visible:text-sm focus-visible:scale-125 focus-visible:border-2 focus-visible:border-stone-900 focus-visible:z-10',
+        { 'hover:scale-150': copied }
       )}
       onClick={handleClick}
       style={{ backgroundColor: color, color: textColor.alpha(0.5).toHslString() }}
